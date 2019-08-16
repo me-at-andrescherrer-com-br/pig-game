f44614e37421c7201f748c269f57dd119e117699
André @ CS <andre@computerscience.com.br>
2019-08-16 02:23:24 -0300

    Updated final score

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
       scores[activePlayer];
 
     // Check if player won the game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       document.querySelector("#name-" + activePlayer).textContent = "Winner!";
       document.querySelector(".dice").style.display = "none";
       document
